3135ba9562c5ddd21b8a11a0189f3ba921be97b7
c s sachindra <113860300+sandilya27@users.noreply.github.com>
2024-08-03 14:07:33 +0530

    created the fe and auth page

---
+++ client/src/pages/auth/index.tsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+
+import Background from "@/assets/empty.png";
+import Victory from "@/assets/victory.svg";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const Auth = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  const handleLogin = async () => {
+    // Handle login logic here
+  };
+
+  const handleSignup = async () => {
+    // Handle signup logic here
+  };
+
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center">
+              <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
+              <img src={Victory} alt="Victory Emoji" className="h-[100px]" />
+            </div>
+            <p className="font-medium text-center">
+              Fill in the details to get started with the best chat app!
+            </p>
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <Tabs defaultValue="login" className="w-3/4">
+              <TabsList className="bg-transparent rounded-none w-full">
+                <TabsTrigger value="login" className="tabTrigger">
+                  Login
+                </TabsTrigger>
+                <TabsTrigger value="signup" className="tabTrigger">
+                  Signup
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="login" className="flex flex-col gap-5 mt-10">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button className="rounded-full p-6" onClick={handleLogin}>
+                  Login
+                </Button>
+              </TabsContent>
+              <TabsContent value="signup" className="flex flex-col gap-5">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Input
+                  placeholder="Confirm Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <Button className="rounded-full p-6" onClick={handleSignup}>
+                  Signup
+                </Button>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+        <div className="hidden xl:flex justify-center items-center">
+          <img src={Background} alt="background of login" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
