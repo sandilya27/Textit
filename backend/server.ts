94b4549746aeafee04786e842a1e02a08f57c8b3
c s sachindra <113860300+sandilya27@users.noreply.github.com>
2024-08-04 11:48:56 +0530

    express setup

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,11 @@
-console.log(`welcome my friend welcome`)
+import app from "./src/app";
+
+const startServer = () => {
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`Listening on the port: ${port}`)
+    })
+}
+
+startServer();
