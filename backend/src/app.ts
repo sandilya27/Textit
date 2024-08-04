be91e9157e071220783d9438bed79a880a275384
c s sachindra <113860300+sandilya27@users.noreply.github.com>
2024-08-04 23:41:18 +0530

    added global error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+
 const app = express();
 
 //Routes
 app.get('/', (req, res) => {
     res.json({ message: "Welcome my friend" })
 })
+
+//Global error handler
+app.use(globalErrorHandler);
 
 export default app;
